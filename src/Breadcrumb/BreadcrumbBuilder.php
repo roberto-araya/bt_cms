f3164dfb266be27cb0f87e8f20855865a65ca587
Roberto Araya <roberto.araya@btester.cl>
2018-01-18 12:55:47 -0300

    by roberto_araya: Add class casting to  in BreadcrumbsBuilder constructor.

--- src/Breadcrumb/BreadcrumbBuilder.php
+++ src/Breadcrumb/BreadcrumbBuilder.php
@@ -6,6 +6,7 @@ use Drupal\Core\Breadcrumb\Breadcrumb;
 use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Link;
+use Drupal\Core\Config\ConfigFactory;
 
 /**
  * Class BreadcrumbBuilder.
@@ -47,7 +48,7 @@ class BreadcrumbBuilder implements BreadcrumbBuilderInterface {
   /**
    * Class constructor.
    */
-  public function __construct($configFactory) {
+  public function __construct(ConfigFactory $configFactory) {
     $this->siteName = $configFactory->get('system.site')->get('name');
   }
 
